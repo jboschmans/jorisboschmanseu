c5d5a25b5e03de8a8d937af404de7bb181ef0fbd
Joris Boschmans <jorisboschmans@gmail.com>
2017-03-16 22:43:25 +0100

    Start of first page + RouteController

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Joris Boschmans</title>
+  <link rel="stylesheet" type="text/css" href="https://bootswatch.com/flatly/bootstrap.min.css" />
+</head>
+<body>
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">Joris Boschmans</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container" style="margin-top:50px;">
+    <h1>Welcome to the homepage!</h1>
+  </div>
+</body>
+</html>
